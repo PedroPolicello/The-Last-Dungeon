1ec93f8d0e385ec966a12493af095cce7f0c9435
cavernadormaster <89394365+cavernadormaster@users.noreply.github.com>
2022-06-14 10:41:56 -0300

    Add Full Player Script
    terminei o Script do player

--- The Last Dungeon/Assets/Scripts/PlayerControl.cs
+++ The Last Dungeon/Assets/Scripts/PlayerControl.cs
@@ -5,22 +5,21 @@ using UnityEngine.UI;
 
 public class PlayerControl : MonoBehaviour
 {
-  [Header("Player Health")]
   public HealthControl healthbar;
-  
   public GameObject dummyCam;
- 
+
   Rigidbody rgb;
   private int _maxhealth = 100;
   private int _currentHealth;
   private bool _isOnGround; 
   private Vector3 _move;
   private Vector3 _rotate; 
+  private float _forcemove=1000;
+  private float _drag=4;
   
   public enum States
   {
-     Walk,
-     ATACCK,
+     Atacck,
      Jump,
      Collect,
      Damage,
@@ -33,14 +32,12 @@ public class PlayerControl : MonoBehaviour
    {
       _currentHealth = _maxhealth;
       healthbar.SetMaxHealth(_maxhealth);
+      rgb = GetComponent<Rigidbody>();
    }
    public void Update()
    {
        switch (state)
        {
-           case States.Walk:
-              Walk();
-              break;
            case States.Idle:
               Idle();
               break;
@@ -49,21 +46,25 @@ public class PlayerControl : MonoBehaviour
         {
           TakeDamage(20);
         }
-         _move = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-         if (dummyCam) 
-         state = States.Walk;
-         
-
-   }
-   void Walk()
-   {
-      _move = dummyCam.transform.TransformDirection(_move);
-       _move = new Vector3(_move.x,0,_move.z);
-        if (_move.magnitude > 0) //&& !_isOnGround )
+        _move = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        if (dummyCam) 
+            _move = dummyCam.transform.TransformDirection(_move);
+       
+        _move = new Vector3(_move.x,0,_move.z);
+        if (_move.magnitude > 0 && _isOnGround )
         {
             transform.forward = Vector3.Slerp(transform.forward,_move,Time.deltaTime*10);
         }
-   }
+    }
+         
+ void FixedUpdate()
+    {
+        float vel = rgb.velocity.magnitude;
+        rgb.AddForce((_move * _forcemove)/ (vel*2+1));
+        Vector3 velwoy = new Vector3(rgb.velocity.x, 0, rgb.velocity.z);
+        rgb.AddForce(-velwoy * _drag);
+    }
+   
    void Idle()
    {
 
